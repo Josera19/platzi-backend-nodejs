4bd49f32880e3c74088b8908b8f1ad7e9b8c3d1b
Alejos Jr <josera.a19@gmail.com>
2025-03-20 14:14:52 -0400

    Latest Config

--- api/index.js
+++ api/index.js
@@ -5,12 +5,13 @@ const routerAPI = require('./routers/index');
 const { errorHandler, logErrors, boomErrorHandler } = require('./middlewares/errorHandler');
 const app = express();
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-const whiteList = ['http://localhost:8080', 'https://myapp.com'];
+const whiteList = ['http://localhost:3001/api', 'https://platzi-backend-nodejs.vercel.app/'];
 const options = {
   origin : (origin, callback) => {
-    if(whiteList.includes(origin)){
+    console.log('origin', origin);
+    if(whiteList.includes(origin) || !origin){
       callback(null, true);
     } else {
       callback(new Error('Not Allowed'));
@@ -18,14 +19,16 @@ const options = {
   }
 }
 
-app.use(cors(options));
+
 app.use(express.json());
 
-app.get('/' , (req, res) => {
+app.get('/api/' , (req, res) => {
   res.send('My first app in express');
 });
 
 routerAPI(app);
+//si colocas el : app.use(cors(options)); antes de la línea : routerApi(app); bloquea todo, hasta la petición desde el localhost:3000/api/v1/products
+app.use(cors(options));
 // Utilizamos los middleware. Siempre deben ir después del routing:
 app.use(logErrors);
 app.use(boomErrorHandler);
